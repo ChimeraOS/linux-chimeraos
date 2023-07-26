57eca498616a34462e4f3602927aba458934a5de
Joaquín Ignacio Aramendía <samsagax@gmail.com>
2023-07-26 09:10:50 -0300

    make-tag.sh: Add a check for clean tree before allowing a tag
    [skip ci]

--- make-tag.sh
+++ make-tag.sh
@@ -3,5 +3,10 @@
 . linux/PKGBUILD
 
 tagname="v${pkgver%.*}-${pkgver##*.}-${pkgrel}"
-echo "Making git tag for $tagname"
-git tag "${tagname}" && git log -1
+if out=$(git status --porcelain) && [ -z "$out" ]; then
+  echo "Making git tag for $tagname"
+  git tag "${tagname}" && git log -1
+else
+  echo "Working directory is not clean"
+  git status
+fi
